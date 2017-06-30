635730b1ba33e9152c976c4b2e3160ad7f4dbd0b
Casey Law <caseyjlaw@gmail.com>
2017-06-30 16:44:41 -0600

    warns if no bdf found

--- sdmpy/bdf.py
+++ sdmpy/bdf.py
@@ -75,6 +75,7 @@ class BDF(object):
             self.fp = open(fname, 'r')
         except IOError:
             self.fp = None
+            logger.warn('No BDF file found at {0}'.format(fname))
         else:
             self.mmdata = mmap.mmap(self.fp.fileno(), 0, mmap.MAP_PRIVATE,
                                     mmap.PROT_READ)
