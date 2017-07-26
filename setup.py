817c814e702a294f684e28974fd6003a85a412a9
Casey J Law <caseyjlaw@gmail.com>
2017-07-26 06:48:31 -0700

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'sdmpy',
-    version='1.36',
+    version='1.4',
     description='Python for ALMA/VLA Science Data Model',
     author='Paul Demorest',
     author_email='pdemores@nrao.edu',
