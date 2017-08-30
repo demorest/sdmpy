f617210b1336b358ded528a4dca8ee5ed015757c
Casey Law <caseyjlaw@gmail.com>
2017-08-30 11:30:00 -0600

    adding 0 at version end to make sure latest is used by pip

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'sdmpy',
-    version='1.4',
+    name='sdmpy',
+    version='1.40',
     description='Python for ALMA/VLA Science Data Model',
     author='Paul Demorest',
     author_email='pdemores@nrao.edu',
     url='http://github.com/demorest/sdmpy',
-    packages = find_packages(),        # get all python scripts in realtime
+    packages=find_packages(),        # get all python scripts in realtime
     install_requires=['lxml', 'numpy'],
-    package_data={'sdmpy':['xsd/*.xsd',]},
+    package_data={'sdmpy': ['xsd/*.xsd']},
     scripts=['scripts/bdf_average.py',
-        'scripts/bdf_bin_split.py',
-        'scripts/bdf_dedisperse_cut.py',
-        'scripts/bdf_to_psrfits.py',
-        ]
+             'scripts/bdf_bin_split.py',
+             'scripts/bdf_dedisperse_cut.py',
+             'scripts/bdf_to_psrfits.py',
+             ]
 )
