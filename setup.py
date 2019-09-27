101fb60f3967b4cea0569868f8e5dba908d89163
Paul Demorest <pdemores@nrao.edu>
2019-09-27 09:55:42 -0600

    setup.py: add realfast_sdm_fix, version bump.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sdmpy',
-    version='1.70.3',
+    version='1.70.4',
     description='Python for ALMA/VLA Science Data Model',
     author='Paul Demorest',
     author_email='pdemores@nrao.edu',
@@ -20,5 +20,6 @@ setup(
              'scripts/bdf_bin_split.py',
              'scripts/bdf_dedisperse_cut.py',
              'scripts/bdf_to_psrfits.py',
+             'scripts/realfast_sdm_fix.py',
              ]
 )
