53ccf2030b428b670c499d8c14b11de729b735d8
Paul Demorest <pdemores@nrao.edu>
2020-07-01 09:37:08 -0600

    Version 1.70.7

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='sdmpy',
-    version='1.70.6',
+    version='1.70.7',
     description='Python for ALMA/VLA Science Data Model',
     author='Paul Demorest',
     author_email='pdemores@nrao.edu',
     url='http://github.com/demorest/sdmpy',
     packages=find_packages(),        # get all python scripts in realtime
-    install_requires=['lxml', 'numpy', 'future', 'progressbar'],
+    install_requires=['lxml', 'numpy', 'future', 'progressbar2'],
     package_data={'sdmpy': ['xsd/*.xsd']},
     scripts=['scripts/bdf_average.py',
              'scripts/bdf_bin_split.py',
