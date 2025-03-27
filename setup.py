3604020ee1e96996d869571f1286cbab763265e1
Paul Demorest <pdemores@nrao.edu>
2025-03-27 14:46:58 -0600

    Version 1.71.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sdmpy',
-    version='1.71.1',
+    version='1.71.2',
     description='Python for ALMA/VLA Science Data Model',
     author='Paul Demorest',
     author_email='pdemores@nrao.edu',
